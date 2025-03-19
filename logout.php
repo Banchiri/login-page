36876033aabf3a61740544aea3b9128f1a79f3aa
sandrabanchiri <sandrabanchiri39@gmail.com>
2025-03-19 12:47:37 +0300

    adds a secure measure in the logout.php file ie session start, session unset for the variables, session destroy and redirects the user to the login.php file after the the destroy and exists

--- logout.php
+++ logout.php
@@ -1,6 +1,9 @@
 <?php
-session_destroy();
-header('location: index.php");
+session_start();
+session_unset();//Unset all session variables
+session_destroy();//destroy the session
+header("location: index.php"); // redirect the user to login.php or any other login page
+exit()
 
 
 ?>
