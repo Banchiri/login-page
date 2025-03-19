f7c2c07661b13820d4d0a438b1910afa81dee632
sandrabanchiri <sandrabanchiri39@gmail.com>
2025-03-19 13:04:42 +0300

    adds error handling property to the connect.php file that makes sure the script stops when the connection fails

--- connect.php
+++ connect.php
@@ -5,6 +5,7 @@ $pass="";
 $db="login";
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
-    echo "Failed to connect to DB".$conn->connect_error;
+    //echo "Failed to connect to DB" . $conn->connect_error; this does not stop the excution on failure
+    die("Connection failed:" . $conn->connection_error);
 }
 ?>
